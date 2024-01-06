8963ce0992774367f72bc1188e0e5fcf819907eb
Will <yzj_000@163.com>
2024-01-06 13:55:39 +0800

    Parse creds from cli arguments

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	azidentity "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
 )
 
+type TenantCredential struct {
+	tenantId string
+	clientId string
+	username string
+	password string
+}
+
 func main() {
+	tenantCredential := parseArguments()
+	log.Println(*tenantCredential)
+
 	graphClient, err := initGraphClient()
 	log.Println(graphClient)
 	log.Println(err)
+}
+
+func parseArguments() *TenantCredential {
+	tenantId := flag.String("tid", "tenant_id", "Tenant name")
+	clientId := flag.String("cid", "client_id", "Client ID")
+	username := flag.String("user", "username", "Username")
+	password := flag.String("pwd", "password", "Password")
+	flag.Parse()
+	
+	tenantCredential := TenantCredential{*tenantId, *clientId, *username, *password}
+	return &tenantCredential
 }
 
 func initGraphClient() (*msgraphsdk.GraphServiceClient, error) {
