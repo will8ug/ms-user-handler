fceb0ce2d368bb0d1afe10ef8345270cd86e3d34
Will <yzj_000@163.com>
2024-01-11 00:08:07 +0800

    Calculate color based on coverage number

--- generate-coverage.sh
+++ generate-coverage.sh
@@ -8,18 +8,25 @@ then
 fi
 
 coverage="0.0%"
-color=brightgreen
+color=green
 while read line
 do
     if [[ "$line" =~ ^total.*%$ ]]
     then
         coverage=`echo $line | awk '{ print $3 }'`
-        coverage=${coverage%\%}
     fi
 done < $srcFile
+echo Coverage: $coverage
 
-echo
-echo $coverage
-echo "https://img.shields.io/badge/Coverage-${coverage}%25-${color}"
+coverage=${coverage%\%}
+if (( $(echo "$coverage <= 30" | bc -l) )) ; then
+    color=red
+elif (( $(echo "$coverage > 80" | bc -l) )); then
+    color=green
+else
+    color=orange
+fi
+
+echo "Downloading https://img.shields.io/badge/Coverage-${coverage}%25-${color}"
 url="https://img.shields.io/badge/Coverage-${coverage}%25-${color}"
 curl -kf -m 10 --create-dirs -o badges/badge-statements.svg $url
